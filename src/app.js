d56f52c50cd1bcc82a5047870a5a7cc9ec8efb66
Kathrin Ulmer <60884247+Kathrin92@users.noreply.github.com>
2020-03-06 15:43:58 +0100

    Searchbar (#2)
    * Implement searchbar-function with div-container
    
    * Add searchbar styling
    
    * Implement search bar to main content

--- src/app.js
+++ src/app.js
@@ -2,14 +2,17 @@ import './app.scss';
 import { title } from './components/header';
 import { createElement } from './lib/dom';
 import Logo from './assets/logotv.svg';
+import { searchBar } from './components/search';
 
 export function app() {
   const header = createElement('header', { className: 'header' });
   const main = createElement('main');
   const titleElement = title('Movie-Maze');
   const logo = createElement('img', { className: 'logo', src: Logo });
+  const searchElement = searchBar();
 
   header.appendChild(logo);
   header.appendChild(titleElement);
+  main.appendChild(searchElement);
   return [header, main];
 }
