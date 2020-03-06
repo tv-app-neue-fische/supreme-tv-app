2b676b2d05be607eecc41be95d8e44c29fa329b1
Caruki <caruki91@posteo.de>
2020-03-06 13:12:40 +0100

    Add basic styling and logo

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import './app.scss';
 import { title } from './components/header';
 import { createElement } from './lib/dom';
+import Logo from '../assets/logotv.svg';
 
 export function app() {
   const header = createElement('header', { className: 'header' });
   const titleElement = title('Movie-Maze');
+  const logo = createElement('img', { className: 'logo', src: Logo });
+
   header.appendChild(titleElement);
+  header.appendChild(logo);
   return header;
 }
